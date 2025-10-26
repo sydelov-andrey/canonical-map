e7391719ebdb1e768a9e86b4792db2c6e72f94fc
Andrey Sydelov 2022 <spb.sidelev@gmail.com>
2025-10-26 22:08:59 +0200

    Create s-canonical-map.js

---
+++ s-canonical-map.js
@@ -0,0 +1,10 @@
+window.__canonicalMap = {
+  "/de-blog/kubernetes-foundations-architecture-core-components":
+    "https://idatamax.com/blog/kubernetes-foundations-architecture-core-components",
+  "/de-blog/kubernetes-under-the-hood-mechanisms-networking":
+    "https://idatamax.com/blog/kubernetes-under-the-hood-mechanisms-networking",
+  "/de-blog/kubernetes-in-depth-storage-security-features":
+    "https://idatamax.com/blog/kubernetes-in-depth-storage-security-features",
+  "/de-blog/kubernetes-best-practices-deployment-troubleshooting":
+    "https://idatamax.com/blog/kubernetes-best-practices-deployment-troubleshooting"
+};
