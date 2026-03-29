939d9c1fcf45461793a1dd63c4a6074745f4f389
Andrey Sydelov <spb.sidelev@gmail.com>
2026-03-29 22:26:40 +0300

    Create m-canonical-map.js

---
+++ m-canonical-map.js
@@ -0,0 +1,10 @@
+window.__canonicalMap = {
+  "/blog/pca-transforms-high-dimensional-data":
+    "https://sydelov.com/ai-blog/pca-transforms-high-dimensional-data",
+  "/blog/neural-networks":
+    "https://sydelov.com/ai-blog/neural-networks",
+  "/blog/convolutional-neural-networks":
+    "https://sydelov.com/ai-blog/convolutional-neural-networks",
+  "/blog/transformers-architecture-and-operating-principles":
+    "https://sydelov.com/ai-blog/transformers-architecture-and-operating-principles"
+};
